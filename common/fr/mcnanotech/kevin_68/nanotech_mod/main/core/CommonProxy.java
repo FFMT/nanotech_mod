6ac7641919262cf92b9c227343a2d15b4294642d
robin <robin4002@gmail.com>
2013-10-22 14:43:13 +0200

    useless import

--- common/fr/mcnanotech/kevin_68/nanotech_mod/main/core/CommonProxy.java
+++ common/fr/mcnanotech/kevin_68/nanotech_mod/main/core/CommonProxy.java
@@ -1,9 +1,5 @@
 package fr.mcnanotech.kevin_68.nanotech_mod.main.core;
 
-import cpw.mods.fml.common.registry.TickRegistry;
-import cpw.mods.fml.relauncher.Side;
-import fr.mcnanotech.kevin_68.nanotech_mod.main.utils.NanotechServerTickHandler;
-
 public class CommonProxy
 {
 	public void registerModRenders()
