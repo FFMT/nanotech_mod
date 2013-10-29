a7cbfb4a1838240e713dac4d512f8edb0e90537f
robin <robin4002@gmail.com>
2013-10-29 17:23:34 +0100

    Fix param name

--- common/fr/mcnanotech/kevin_68/nanotech_mod/main/items/ItemNitrogenBucket.java
+++ common/fr/mcnanotech/kevin_68/nanotech_mod/main/items/ItemNitrogenBucket.java
@@ -12,39 +12,36 @@ public class ItemNitrogenBucket extends ItemBucket
 	{
 		super(id, fluidId);
 	}
-	
-	public boolean tryPlaceContainedLiquid(World par1World, int par2, int par3, int par4)
-    {
-            Material material = par1World.getBlockMaterial(par2, par3, par4);
-            boolean flag = !material.isSolid();
 
-            if (!par1World.isAirBlock(par2, par3, par4) && !flag)
-            {
-                return false;
-            }
-            else
-            {
-                if (par1World.provider.isHellWorld)
-                {
-                    par1World.playSoundEffect((double)((float)par2 + 0.5F), (double)((float)par3 + 0.5F), (double)((float)par4 + 0.5F), "random.fizz", 0.5F, 2.6F + (par1World.rand.nextFloat() - par1World.rand.nextFloat()) * 0.8F);
+	public boolean tryPlaceContainedLiquid(World world, int x, int y, int z)
+	{
+		Material material = world.getBlockMaterial(x, y, z);
+		boolean flag = !material.isSolid();
 
-                    for (int l = 0; l < 8; ++l)
-                    {
-                        par1World.spawnParticle("largesmoke", (double)par2 + Math.random(), (double)par3 + Math.random(), (double)par4 + Math.random(), 0.0D, 0.0D, 0.0D);
-                    }
-                }
-                else
-                {
-                    if (!par1World.isRemote && flag && !material.isLiquid())
-                    {
-                        par1World.destroyBlock(par2, par3, par4, true);
-                    }
+		if(!world.isAirBlock(x, y, z) && !flag)
+		{
+			return false;
+		}
+		else
+		{
+			if(world.provider.isHellWorld)
+			{
+				world.playSoundEffect((double)((float)x + 0.5F), (double)((float)y + 0.5F), (double)((float)z + 0.5F), "random.fizz", 0.5F, 2.6F + (world.rand.nextFloat() - world.rand.nextFloat()) * 0.8F);
 
-                    par1World.setBlock(par2, par3, par4, NanotechBlock.BlockLiquidNitrogen.blockID, 0, 3);
-                }
-
-                return true;
-            }
-        
-    }
+				for(int l = 0; l < 8; ++l)
+				{
+					world.spawnParticle("largesmoke", (double)x + Math.random(), (double)y + Math.random(), (double)z + Math.random(), 0.0D, 0.0D, 0.0D);
+				}
+			}
+			else
+			{
+				if(!world.isRemote && flag && !material.isLiquid())
+				{
+					world.destroyBlock(x, y, z, true);
+				}
+				world.setBlock(x, y, z, NanotechBlock.liquidNitrogen.blockID, 0, 3);
+			}
+			return true;
+		}
+	}
 }
