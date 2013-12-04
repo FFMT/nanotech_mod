30a5d355f76ca1674ec38985acc27057b28c5da5
robin <robin4002@gmail.com>
2013-12-04 23:45:24 +0100

    Smoker is better like it

--- common/fr/mcnanotech/kevin_68/nanotech_mod/main/blocks/BlockSmoker.java
+++ common/fr/mcnanotech/kevin_68/nanotech_mod/main/blocks/BlockSmoker.java
@@ -21,41 +21,14 @@ public class BlockSmoker extends BlockContainer
 	public void randomDisplayTick(World world, int x, int y, int z, Random random)
 	{
 		TileEntitySmoker tilesmoker = (TileEntitySmoker)world.getBlockTileEntity(x, y, z);
-		float f = (float)x + random.nextFloat();
-		float f1 = (float)y + random.nextFloat() * 0.5F + 0.5F;
-		float f2 = (float)z + random.nextFloat();
 
-		if(tilesmoker.getSmokeValue() == 1)
-			world.spawnParticle("largesmoke", (double)f, (double)f1, (double)f2, 0.0D, 0.1D, 0.0D);
-		if(tilesmoker.getSmokeValue() == 2)
-			world.spawnParticle("largesmoke", (double)f, (double)f2, (double)f2, 0.1D, 0.1D, 0.0D);
-		if(tilesmoker.getSmokeValue() == 3)
-			world.spawnParticle("largesmoke", (double)f, (double)f1, (double)f2, -0.1D, 0.1D, 0.0D);
-		if(tilesmoker.getSmokeValue() == 4)
-			world.spawnParticle("largesmoke", (double)f, (double)f1, (double)f2, 0.0D, 0.1D, 0.1D);
-		if(tilesmoker.getSmokeValue() == 5)
-			world.spawnParticle("largesmoke", (double)f, (double)f1, (double)f2, 0.0D, 0.1D, -0.1D);
-		if(tilesmoker.getSmokeValue() == 6)
-			world.spawnParticle("largesmoke", (double)f, (double)f1, (double)f2, 0.1D, 0.1D, 0.1D);
-		if(tilesmoker.getSmokeValue() == 7)
-			world.spawnParticle("largesmoke", (double)f, (double)f1, (double)f2, -0.1D, 0.1D, -0.1D);
-		if(tilesmoker.getSmokeValue() == 8)
-			world.spawnParticle("largesmoke", (double)f, (double)f1, (double)f2, -0.1D, 0.1D, 0.1D);
-		if(tilesmoker.getSmokeValue() == 9)
-			world.spawnParticle("largesmoke", (double)f, (double)f1, (double)f2, 0.1D, 0.1D, -0.1D);
-		if(tilesmoker.getSmokeValue() == 10)
-			world.spawnParticle("largesmoke", (double)f, (double)f1, (double)f2, 0.0D, 0.15D, 0.0D);
-		if(tilesmoker.getSmokeValue() == 11)
-			world.spawnParticle("largesmoke", (double)f, (double)f1, (double)f2, 0.0D, 0.17D, 0.0D);
-		if(tilesmoker.getSmokeValue() == 12)
-			world.spawnParticle("largesmoke", (double)f, (double)f1, (double)f2, 0.0D, 0.2D, 0.0D);
-		if(tilesmoker.getSmokeValue() == 13)
-			world.spawnParticle("largesmoke", (double)f, (double)f1, (double)f2, 0.0D, 0.25D, 0.0D);
-		if(tilesmoker.getSmokeValue() == 14)
-			world.spawnParticle("largesmoke", (double)f, (double)f1, (double)f2, 0.0D, 0.3D, 0.0D);
-		if(tilesmoker.getSmokeValue() == 15)
-			world.spawnParticle("largesmoke", (double)f, (double)f1, (double)f2, 0.0D, 0.35D, 0.0D);
-
+		for(int i = 0; i < tilesmoker.getSmokeValue() * 2; i++)
+		{
+			float f = (float)x + random.nextFloat();
+			float f1 = (float)y + random.nextFloat() * 0.5F + 0.5F;
+			float f2 = (float)z + random.nextFloat();
+			world.spawnParticle("largesmoke", (double)f, (double)f1, (double)f2, 0.0D, 0.002D * i, 0.0D);
+		}
 	}
 
 	@Override
